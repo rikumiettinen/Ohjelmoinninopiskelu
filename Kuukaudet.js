69f1ad462f13829babbdf920c927d591c092a2e1
rikumiettinen <60388624+rikumiettinen@users.noreply.github.com>
2021-05-19 12:26:08 +0300

    Create Kuukaudet.js

---
+++ Kuukaudet.js
@@ -0,0 +1,44 @@
+function kuukaudet (kk)
+{
+  switch (kk)
+  {
+    case 1;
+      console.log("Tammikuu");
+      break;
+    case 2;
+      console.log("Helmikuu");
+      break;
+    case 3;
+      console.log("Maaliskuu");
+      break;
+    case 4;
+      console.log("Huhtikuu");
+      break;
+    case 5;
+      console.log("Toukokuu");
+      break;
+    case 6;
+      console.log("Kesäkuu");
+      break;
+    case 7;
+      console.log("Heinäkuu");
+      break;
+    case 8;
+      console.log("Elokuu");
+      break;
+    case 9;
+      console.log("Syyskuu");
+      break;
+    case 10;
+      console.log("Lokakuu");
+      break;
+    case 11;
+      console.log("Marraskuu");
+      break;
+    case 12;
+      console.log("Joulukuu");
+      break;
+    default:
+      console.log("Et antanut arvoa väliltä 1-12")
+  }
+}
