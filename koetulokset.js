e1b608855217d1cd985d5995148f70ca1c16fa7b
rikumiettinen <60388624+rikumiettinen@users.noreply.github.com>
2021-05-19 13:02:11 +0300

    Create koetulokset.js

---
+++ koetulokset.js
@@ -0,0 +1,27 @@
+function koetulokset(nimi, koe)
+{
+  if(koe < 50)
+  {
+    console.log(nimi + " Hylätty");
+  }
+  else if(koe < 60)
+  {
+    console.log(nimi + " T1");
+  }
+  else if(koe < 70)
+  {
+    console.log(nimi + " T2");
+  }
+  else if(koe < 80)
+  {
+    console.log(nimi + " H3");
+  }
+  else if(koe < 90)
+  {
+    console.log(nimi + " H4");
+  }
+  else {
+  {
+    console.log(nimi + " K5");
+  }
+}
